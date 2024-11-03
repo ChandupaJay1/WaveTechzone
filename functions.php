1346ea869dc6aa474c044c0ab291f12fb646d9c4
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-04 02:52:43 +0530

    optimized dd function, add new function: asset

--- functions.php
+++ functions.php
@@ -1,7 +1,18 @@
 <?php
 
-function dd($var)
+require_once __DIR__ . '/config.php';
+
+function dd(...$var): never
 {
-    print_r($var);
+    echo '<pre>';
+    var_dump(...$var);
+    echo '</pre>';
     die;
+}
+
+function asset($file): string
+{
+    $file = str_starts_with($file, '/') ? $file : '/' . $file;
+    $uri = ROOT . '/assets' . $file;
+    return $uri;
 }
