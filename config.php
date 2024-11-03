f847cafd93e11401ebee0666e615df6bf1e13977
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-04 02:54:34 +0530

    define ROOT uri, and SERVER_ROOT constants

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/functions.php';
 
 // Load environment variables from .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -18,3 +19,10 @@ $dbPass = $_ENV['DB_PASSWORD'];
 if (empty($dbHost) || empty($dbName) || empty($dbUser) || empty($dbPass)) {
     die("Database configuration variables are not set.");
 }
+
+define("SERVER_ROOT", __DIR__);
+
+$dirArr = explode("/", str_replace("\\", "/", __DIR__));
+$rootDirName = $server->rootDirName ?? $dirArr[count($dirArr) - 1];
+$rootHasDirName = str_contains(strtolower($_SERVER["REQUEST_URI"]), strtolower($rootDirName));
+define("ROOT", $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["SERVER_NAME"] . ($rootHasDirName ? "/" . strtolower($rootDirName) : ''));
