f8c5c073ab696ec29eb38fbc32d07c8c05debfbb
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-11-03 21:49:22 +0530

    some changes

--- components/header.php
+++ components/header.php
@@ -1,31 +1,30 @@
 <header class="header-area header-sticky">
     <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav class="main-nav">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
 
-            <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
-              <img src="assets/images/logo-tp.png" alt="" style="width: 50px;">
-            </a>
-            <!-- ***** Logo End ***** -->
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo-tp.png" alt="" style="width: 50px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
 
-            <!-- ***** Menu Start ***** -->
-            <ul class="nav">
-                <li><a href="" <?= !isset($path) || ($path === '/') ? 'class="active"' : '' ?> >Home</a></li>
-                <li><a href="shop" <?= (isset($path) && $path === '/shop') ? 'class="active"' : '' ?> >Shop</a></li>
-                              <!-- <li><a href="/wavetechzone/product-details">Product Details</a></li> -->
-                <li><a href="games" <?= (isset($path) && $path === '/games') ? 'class="active"' : '' ?> >Games</a></li>
-                <li><a href="contact" <?= (isset($path) && $path === '/contact') ? 'class="active"' : '' ?> >Contact Us</a></li>
-                <li><a href="admin" <?= (isset($path) && $path === '/contact') ? 'class="active"' : '' ?> >Admin</a></li>
-              <!-- <li><a href="#">Sign In</a></li> -->
-            </ul>
-            <a class='menu-trigger'>
-              <span>Menu</span>
-            </a>
-            <!-- ***** Menu End ***** -->
-          </nav>
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="" <?= !isset($path) || ($path === '/') ? 'class="active"' : '' ?>>Home</a></li>
+                        <li><a href="shop" <?= (isset($path) && $path === '/shop') ? 'class="active"' : '' ?>>Shop</a></li>
+                        <!-- <li><a href="/wavetechzone/product-details">Product Details</a></li> -->
+                        <li><a href="contact" <?= (isset($path) && $path === '/contact') ? 'class="active"' : '' ?>>Contact Us</a></li>
+                        <li><a href="admin" <?= (isset($path) && $path === '/contact') ? 'class="active"' : '' ?>>Admin</a></li>
+                        <!-- <li><a href="#">Sign In</a></li> -->
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
         </div>
-      </div>
     </div>
-  </header>
+</header>
