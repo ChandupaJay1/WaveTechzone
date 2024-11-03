f16f21acde173696ce281c3816c5d8738487e453
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-04 02:51:54 +0530

    added possible alternative uri paths

--- router.php
+++ router.php
@@ -16,31 +16,42 @@ switch ($path) {
         require 'index.php';
         break;
     case '/contact':
+    case '/contact/':
         require 'contact.php';
         break;
     case '/shop':
+    case '/shop/':
         require 'shop.php';
         break;
+    case '/cart':
+    case '/cart/':
+        require 'cart.php';
+        break;
     case '/games':
+    case '/games/':
         require 'games.php';
         break;
     case '/product-details':
+    case '/product-details/':
         require 'product-details.php';
         break;
+    case '/admin':
     case '/admin/':
         require 'admin/adminLogin.php';
         break;
-    case '/adminDashboard.php':
+    case '/adminDashboard':
+    case '/adminDashboard/':
         require 'admin/adminDashboard.php';
         break;
     case '/adminLogout':
+    case '/adminLogout/':
         require 'admin/adminLogout.php';
         break;
+    case '/adminRegister':
     case '/adminRegister/':
         require 'admin/adminRegister.php';
         break;
     default:
         http_response_code(404);
-        echo "404 - Page Not Found";
-        break;
+        exit("Page not found");
 }
