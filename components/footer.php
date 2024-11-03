72ed72cf163458bfedfc830a6bd0a8d3b73c74c9
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-11-03 22:16:30 +0530

    some changes footer

--- components/footer.php
+++ components/footer.php
@@ -1,24 +1,28 @@
+<link rel="stylesheet" href="assets/css/templatemo-lugx-gaming.css">
+
 <footer>
-  <div class="container">
-    <div class="footer-links">
-      <a href="#">About Us</a>
-      <a href="#">Services</a>
-      <a href="#">Privacy Policy</a>
-      <a href="#">Contact</a>
-    </div>
+    <div class="container">
+        <div class="footer-links">
+            <a href="#">About Us</a>
+            <a href="#">Services</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Contact</a>
+        </div>
 
-    <div class="social-icons">
-      <a href="#" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
-      <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-      <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+        <div class="social-icons">
+            <a href="#" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
+            <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+            <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
 
-    </div>
-    <p>Copyright © 2024 <?= $_ENV['APP_NAME']; ?>. All rights reserved.</p>
-    <p>
-      <a rel="nofollow" href="https://www.facebook.com/chandupajayalath2/" target="_blank">
-        Design And Published By NerdTech Software Company.
-        <img src="./assets/images/nerdtech.png" alt="NerdTech Logo" class="ft-logo">
-      </a>
-    </p>
-  </div>
+            <div class="text-center">
+                <p>
+                    <a rel="nofollow" href="https://www.facebook.com/chandupajayalath2/" target="_blank">
+                        Designed And Published By NerdTech Software Development LK.
+                        <img src="./assets/images/nerdtech.png" alt="NerdTech Logo" class="ft-logo">
+                    </a>
+                </p>
+            </div>
+
+
+
 </footer>
